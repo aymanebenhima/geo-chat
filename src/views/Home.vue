de6c83df1e2b4020a1a597c0f257ada4f7e12c9d
Aymane Benhima <aymanebenhima@gmail.com>
2021-08-28 17:02:00 +0100

    initial template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="welcome container">
+    <div class="card">
+      <div class="card-content center-align">
+        <h2 class="teal-text">Welcome</h2>
+        <form @submit.prevent="enterChat">
+          <label for="name">Enter your username:</label>
+          <input type="text" name="name" v-model="name" />
+          <p v-if="feedback" class="red-text">{{ feedback }}</p>
+          <button class="btn teal">Enter chat</button>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+    data() {
+        return {
+            name: null,
+            feedback: null,
+        }
+    },
+    methods: {
+        enterChat () {
+            if (this.name) {
+                this.$router.push({ name: 'Chat', params: { name: this.name } });
+            } else {
+                this.feedback = "You must enter the name to join";
+            }
+        }
+    },
+};
 </script>
+
+<style>
+.welcome {
+    max-width: 400px !important;
+    margin-top: 100px !important;
+}
+.welcome h2 {
+    font-size: 3em;
+}
+.welcome button {
+    margin: 30px auto;
+}
+</style>
